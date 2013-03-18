6acaabb135f18ad38d36c62d7c11f572e56407bb
Abhishek Bose <abhishekbose87@gmail.com>
2013-03-18 20:06:25 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oscarwebsite::Application.config.secret_token = '20ed5c808ad5f8959fa9658023f1bcf5dffd1728e6fe792ddfe314dd6292e3fb9d88351be46e70357b5ceedfea9ce82a4dcbbad468da0d05d30e928f3d17503f'
