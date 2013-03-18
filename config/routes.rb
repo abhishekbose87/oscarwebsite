c6884c85f85f70f766a94f9bba1a9f6eface4891
Abhishek Bose <abhishekbose87@gmail.com>
2013-03-18 21:12:40 +0530

    Basic website

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Oscarwebsite::Application.routes.draw do
+  root :to => 'pages#home'
+
+  get 'about' => 'pages#about'
+
+  #get "pages/about"
+
+  #get "pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
